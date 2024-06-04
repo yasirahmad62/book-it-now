58f0e4ec2bd2a6adb0de34b118a7526950176605
Abdullahi <abdullahiamin93@gmail.com>
2024-06-04 12:30:15 -0400

    modify app.js and link the event pages route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import EventListingPage from './EventListingPage';
+import EventBookingPage from './EventBookingPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<EventListingPage />} />
+          <Route path="/book/:id" element={<EventBookingPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
